1fc3ec61da36205ad178c0223c26e13dc4995a9c
Helge Mathee <helge.mathee@fabricengine.com>
2017-03-17 12:21:45 +0100

    refactored rapid xml to kludge

---
+++ 03_kludge/04_rapidxml/RapidXML.tests.kludge.py
@@ -0,0 +1,7 @@
+# encoding: utf-8
+
+ext_MyOwnedType.add_test("""
+MyOwnedType a;
+MyOwnedType b = a;
+""", """
+""")
--- 03_kludge/04_rapidxml/RapidXml.cpp
+++
@@ -1,5 +0,0 @@
-#include "RapidXml.h"
-
-using namespace Fabric::EDK;
-IMPLEMENT_FABRIC_EDK_ENTRIES( RapidXml )
-
